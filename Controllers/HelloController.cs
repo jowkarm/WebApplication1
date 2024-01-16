58f1e0eba0edcd0c623add02af4c2029922e4616
jowkarm <jokar.mehdi2@gmail.com>
2024-01-16 12:57:49 -0800

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet(Name = "GetHelloWorld")]
+        public string HelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents()
+        {
+            return "100 students";
+        }
+
+
+        [HttpGet("{id}")]
+        public string GetOneStudent(int id)
+        {
+            return "Student Number: " + id;
+        }
+
+        [HttpGet("mehdi")]
+        public string GetMehdi()
+        {
+            return "Mehdi Jokar";
+        }
+    }
+}
