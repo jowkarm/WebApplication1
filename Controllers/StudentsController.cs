38365d5052e0b2a8cd918748b0ded97d693e00c7
jowkarm <jokar.mehdi2@gmail.com>
2024-01-23 11:45:53 -0800

    cleaning the code

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -7,23 +8,46 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
+
+        private List<Student> students = new List<Student>{
+            new Student(0, "William", "Taylor", "william.taylor@example.com", "333-444-5555"),
+            new Student(1, "John", "Doe", "john.doe@example.com", "123-456-7890"),
+            new Student(2, "Jane", "Smith", "jane.smith@example.com", "987-654-3210"),
+            new Student(3, "Alice", "Johnson", "alice.johnson@example.com", "555-123-4567"),
+            new Student(4, "Bob", "Williams", "bob.williams@example.com", "444-789-0123"),
+            new Student(5, "Eva", "Brown", "eva.brown@example.com", "789-012-3456"),
+            new Student(6, "Michael", "Lee", "michael.lee@example.com", "111-222-3333"),
+            new Student(7, "Olivia", "Miller", "olivia.miller@example.com", "777-888-9999"),
+            new Student(8, "Daniel", "Davis", "daniel.davis@example.com", "222-333-4444"),
+            new Student(9, "Sophia", "Moore", "sophia.moore@example.com", "666-777-8888")
+        };
+
+
+
+
         [HttpGet]
-        public string GetStudents()
+        public List<Student> GetStudents()
         {
-            return "100 students";
+            return students;
         }
 
 
         [HttpGet("{id}")]
-        public string GetOneStudent(int id)
+        public Student GetOneStudent(int id)
         {
-            return "Student Number: " + id;
+            Student student = students[id];
+            return student;
         }
 
-        [HttpGet("mehdi")]
-        public string GetMehdi()
+        [HttpPost("add-student")]
+        public List<Student> AddStudent([FromBody] Student newStudent)
         {
-            return "Mehdi Jokar";
+            students.Add(newStudent);
+
+            return students;
+
         }
+
+        
     }
 }
---
+++ Models/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+
+        // Default constructor
+        public Student()
+        {
+        }
+
+        // Parameterized constructor
+        public Student(int id, string firstName, string lastName, string email, string phone)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+        }
+    }
+
+}
