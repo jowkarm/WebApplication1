260d12edfe0a9160ca0dc6be067b99ac2296fbed
jowkarm <jokar.mehdi2@gmail.com>
2024-02-01 19:35:44 -0800

    Add CaseNote class.

---
+++ Models/CaseNote.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApplication1.Models
+{
+    public class CaseNote
+    {
+        
+        
+        public int Id { get; set; }
+        public bool Status { get; set; }
+        public string Subject { get; set; } = string.Empty;
+        public string Note { get; set; } = string.Empty;
+        public DateTime OpenDate { get; set; }
+        public DateTime DueDate { get; set; }
+        
+        public int StudentId { get; set; }
+
+        [JsonIgnore]
+        public virtual Student Student { get; set; }
+
+        public CaseNote(bool status, string subject, string note, DateTime dueDate)
+        {
+            this.Status = status;
+            this.Subject = subject;
+            this.Note = note;
+            this.DueDate = dueDate;
+            this.OpenDate = DateTime.Now;
+        }
+
+        
+    }
+}
